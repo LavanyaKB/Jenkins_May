6fc515a8121e8f7432cbe97ae853303ad77e730c
Lavanya <lavanyakb19@gmail.com>
2023-05-21 21:26:24 +0100

    Jira 2331- Added more tests

--- src/test/java/uk/axone/devintest/git/test/TestApp.java
+++ src/test/java/uk/axone/devintest/git/test/TestApp.java
@@ -7,6 +7,14 @@ public class TestApp {
     public void test1(){
         System.out.println("Running test1");
     }
+    @Test
+    public void test2(){
+        System.out.println("Running test2");
+    }
+    @Test
+    public void test3(){
+        System.out.println("Running test3");
+    }
 
     private void doSomething(){
         System.out.println("Do something");
